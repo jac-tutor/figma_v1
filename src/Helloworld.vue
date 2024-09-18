cb3224f27f7e6b456466a0bd607eed7c8691c5bc
Jac Wang <lu7766lu7766@gmail.com>
2024-09-18 14:23:25 +0800

    vif

--- src/Helloworld.vue
+++ src/Helloworld.vue
@@ -4,6 +4,12 @@
   <div>{{ arr }}</div>
   <div>{{ obj }}</div>
   <div>{{ fullName }}</div>
+  <div>
+    v-if:
+    <span v-if="number > 10">1</span>
+    <span v-else-if="number < 10">-1</span>
+    <span v-else>0</span>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -15,7 +21,7 @@ const arr = reactive<number[]>([])
 arr.push(1)
 arr.push(23)
 arr.push(5657)
-const obj = reactive<any>({})
+const obj = reactive<Record<string, string>>({})
 obj.a = "A"
 obj.firstName = "Jac"
 obj.lastName = "Wang"
@@ -23,4 +29,7 @@ const fullName = computed(() => `${obj.firstName} ${obj.lastName}`)
 watch(num, function () {
   console.log(num.value)
 })
+
+// if (true) {}
+const number = ref(10)
 </script>
