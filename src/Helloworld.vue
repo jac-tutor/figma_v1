d99e4c0d0f37f8005be29bb7e63f3edcded7db2b
Jac Wang <lu7766lu7766@gmail.com>
2024-09-18 13:33:13 +0800

    ref, reactive, v-model

--- src/Helloworld.vue
+++ src/Helloworld.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>helloworld</div>
+  <div>helloworld {{ 1 + 2 + num }}</div>
+  <input type="text" v-model="num" />
+  <div>{{ arr }}</div>
 </template>
+
+<script setup lang="ts">
+import { reactive, ref } from "vue"
+
+const num = ref("haha")
+num.value = "cc"
+const arr = reactive<number[]>([])
+arr.push(1)
+arr.push(23)
+arr.push(5657)
+</script>
