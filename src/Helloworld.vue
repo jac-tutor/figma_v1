be884c2b689bc6421d871391f90a3a5e5658b3fc
Jac Wang <lu7766lu7766@gmail.com>
2024-09-18 13:53:08 +0800

    computed, watch

--- src/Helloworld.vue
+++ src/Helloworld.vue
@@ -1,12 +1,13 @@
 <template>
-  <div>helloworld {{ 1 + 2 + num }}</div>
+  <div>helloworld {{ num }}</div>
   <input type="text" v-model="num" />
   <div>{{ arr }}</div>
   <div>{{ obj }}</div>
+  <div>{{ fullName }}</div>
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from "vue"
+import { computed, reactive, ref, watch } from "vue"
 
 const num = ref("haha")
 num.value = "cc"
@@ -16,4 +17,10 @@ arr.push(23)
 arr.push(5657)
 const obj = reactive<any>({})
 obj.a = "A"
+obj.firstName = "Jac"
+obj.lastName = "Wang"
+const fullName = computed(() => `${obj.firstName} ${obj.lastName}`)
+watch(num, function () {
+  console.log(num.value)
+})
 </script>
